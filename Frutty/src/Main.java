6ca46a5f6e5cf37098a1d9be9321c7f2243eb8aa
Degubi <Degubi>
2018-04-16 20:45:15 +0200

    Test

--- Frutty/src/Main.java
+++ Frutty/src/Main.java
@@ -5,9 +5,6 @@ import frutty.gui.GuiSettings;
 import frutty.gui.GuiStats;
 
 public class Main {
-	/**
-	 * Fõ program entry pont, Menü JFrame-nek láthatóvá tétele, mentések mappa létrehozása, statok betöltése
-	 */
 	public static void main(String[] args) {
 		GuiMenu.showMenu();
 		GuiSettings.loadSettings();
